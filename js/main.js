49647b816bc502cc8ec26d440ba34f6b72b912a5
Carlos Santana <carlosgabriel46@gmail.com>
2024-09-09 13:55:12 -0600

    Se corrige error de referencia en Index

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
 const contadorProductos = document.getElementById("contadorProductos");
 const productosTotal = document.getElementById("productosTotal");
 const precioTotal = document.getElementById("precioTotal");
-
+const fecha = document.getElementById("fecha");
 
 // Bandera, al ser true permite agregar los datos a la tabla
 let isValid = true;
@@ -170,4 +170,6 @@ window.addEventListener("load", function(){
                 </tr>`;
         cuerpoTabla.insertAdjacentHTML("beforeend", row);
     });
+    // let now = new Date();
+    // fecha.innerText=(now.getMonth()+1) + "/" now.getFullYear();
 });//windows load
